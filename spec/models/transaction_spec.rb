9559319f73f50c3c611c3c2b47937353f7234942
ayumin <ayumu.aizawa@gmail.com>
2010-11-27 11:45:53 +0900

    買掛残高のテストを追加

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,12 +1,20 @@
 require 'spec_helper'
 
 describe Transaction do
-  before do
-    @trn = Transaction.new(:shop_code => "001")
-  end
+
   context "店舗コード" do
+    before do
+      @trn = Transaction.new(:shop_code => "001")
+    end
     subject { @trn.valid? }
     it { should be_true }
   end
 
+  context "買掛残高" do
+    before do
+      @trn = Transaction.new(:kaizan => "Kaizan.data")
+    end
+    subject { @trn.valid? }
+    it { should be_true }
+  end
 end
