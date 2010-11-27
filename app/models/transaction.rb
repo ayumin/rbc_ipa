452bccf1ca56508618a2e187e17426ced43774ae
ayumin <ayumu.aizawa@gmail.com>
2010-11-27 12:02:52 +0900

    Viewのテストを追加

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,2 +1,3 @@
 class Transaction < ActiveRecord::Base
+  has_attached_file :kaizan, :path => ":rails_root/public/:attachment/:basename_:shop_code.data"
 end
--- spec/views/transactions/new.html.erb_spec.rb
+++ spec/views/transactions/new.html.erb_spec.rb
@@ -13,6 +13,7 @@ describe "transactions/new.html.erb" do
     # Run the generator again with the --webrat-matchers flag if you want to use webrat matchers
     assert_select "form", :action => transactions_path, :method => "post" do
       assert_select "input#transaction_shop_code", :name => "transaction[shop_code]"
+      assert_select "input#transaction_kaizan", :name => "transaction[kaizan]"
     end
   end
 end
