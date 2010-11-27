91fe19675a4b3917c7610f7278448dfe78114c75
ayumin <ayumu.aizawa@gmail.com>
2010-11-27 11:43:26 +0900

    店舗コードのテストを追加

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,5 +1,12 @@
 require 'spec_helper'
 
 describe Transaction do
+  before do
+    @trn = Transaction.new(:shop_code => "001")
+  end
+  context "店舗コード" do
+    subject { @trn.valid? }
+    it { should be_true }
+  end
 
 end
